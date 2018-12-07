40982535e1ead0c7574045ef86564667e56c44cb
Huy Huynh <huyhvq@icloud.com>
2018-12-07 15:07:42 +0700

    Init commit

---
+++ tracing/init.go
@@ -0,0 +1,48 @@
+package tracing
+
+import (
+	"fmt"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go/config"
+	"github.com/uber/jaeger-client-go/rpcmetrics"
+	"github.com/uber/jaeger-lib/metrics"
+	"go.uber.org/zap"
+
+	"github.com/huyhvq/jaeger/log"
+)
+
+// Init creates a new instance of Jaeger tracer.
+func Init(serviceName string, metricsFactory metrics.Factory, logger log.Factory) opentracing.Tracer {
+	cfg, err := config.FromEnv()
+	if err != nil {
+		logger.Bg().Fatal("cannot parse Jaeger env vars", zap.Error(err))
+	}
+	cfg.ServiceName = serviceName
+	cfg.Sampler.Type = "const"
+	cfg.Sampler.Param = 1
+
+	jaegerLogger := jaegerLoggerAdapter{logger.Bg()}
+
+	metricsFactory = metricsFactory.Namespace(serviceName, nil)
+	tracer, _, err := cfg.NewTracer(
+		config.Logger(jaegerLogger),
+		config.Metrics(metricsFactory),
+		config.Observer(rpcmetrics.NewObserver(metricsFactory, rpcmetrics.DefaultNameNormalizer)),
+	)
+	if err != nil {
+		logger.Bg().Fatal("cannot initialize Jaeger Tracer", zap.Error(err))
+	}
+	return tracer
+}
+
+type jaegerLoggerAdapter struct {
+	logger log.Logger
+}
+
+func (l jaegerLoggerAdapter) Error(msg string) {
+	l.logger.Error(msg)
+}
+
+func (l jaegerLoggerAdapter) Infof(msg string, args ...interface{}) {
+	l.logger.Info(fmt.Sprintf(msg, args...))
+}
